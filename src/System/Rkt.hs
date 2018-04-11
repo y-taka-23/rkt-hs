8787af5906450d5e33e26c536b26d3eb46bdf57c
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-04-11 12:40:50 +0900

    Define a datatype for Pods

--- src/System/Rkt.hs
+++ src/System/Rkt.hs
@@ -1,1 +1,33 @@
 module System.Rkt where
+
+import Data.Text
+import Data.Time
+
+data Pod = Pod {
+      podUUID            :: Text
+    , podState           :: PodState
+    , podNetworks        :: [Network]
+    , podAppNames        :: [Text]
+    , podApps            :: [App]
+    , podCreatedAt       :: UTCTime
+    , podStartedAt       :: UTCTime
+    , podUserAnnotations :: [(Text, Text)]
+    , podUserLabels      :: [(Text, Text)]
+    } deriving (Eq, Show)
+
+data PodState =
+      PodEmbryo
+    | PodPreparing
+    | PodAbortedPrepare
+    | PodPrepared
+    | PodRunning
+    | PodDeleting
+    | PodExitedDeleting
+    | PodExited
+    | PodExitedGarbage
+    | PodGarbage
+    deriving (Eq, Show)
+
+-- Todo: stub
+data App = App deriving (Eq, Show)
+data Network = Network deriving (Eq, Show)
